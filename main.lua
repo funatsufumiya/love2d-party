93330a761d267ab564bf3a7815f62ee95d3354c3
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-02-28 14:41:16 +0900

    add star texture

--- main.lua
+++ main.lua
@@ -1,21 +1,28 @@
 local party = require("party")
 local system = party(750)
-	:origin(50, 50)
-	:radius(5, 25)
-	:startColor(0, 0.2, 1)
-	:areaSpread(100, 60)
-	:linearAcceleration(0, 1000)
-	:endColor(0, 0.9, 1, 0)
-	:lifetime(1, 1.2)
-	:startAngle(-math.pi, math.pi)
-	:spin(-2, 2)
-	:direction(math.pi / 2)
-	:speed(250)
+    :origin(50, 50)
+    :radius(5, 25)
+    :startColor(0, 0.2, 1)
+    :areaSpread(100, 60)
+    :linearAcceleration(0, 1000)
+    :endColor(0, 0.9, 1, 0)
+    :lifetime(1, 1.2)
+    :startAngle(-math.pi, math.pi)
+    :spin(-2, 2)
+    :direction(math.pi / 2)
+    :speed(250)
+
+local particleImage
+
+function love.load()
+	particleImage = love.graphics.newImage("particle.png")
+	system:texture(particleImage)
+end
 
 function love.update(dt)
-  system:update(dt)
+	system:update(dt)
 end
 
 function love.draw()
-  system:draw()
+	system:draw()
 end
